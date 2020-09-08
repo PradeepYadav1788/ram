cab36671abb7761c0a22c1816a7d21b342475e17
PradeepYadav1788 <33863962+PradeepYadav1788@users.noreply.github.com>
2020-09-08 20:35:05 +0530

    updated

--- iis.ps1
+++ iis.ps1
@@ -1,32 +1,32 @@
 # install IIS server role
 
- function ExitWithCode { param($exitcode=99) $host.SetShouldExit($exitcode); exit }
+ #function ExitWithCode { param($exitcode=99) $host.SetShouldExit($exitcode); exit }
+ param($code=99)
  
- 
- Install-WindowsFeature -name Web-Server -IncludeManagetTools
+ Install-WindowsFeature -name Web-Server -IncludeManagementTools
  
  if($? -eq "true"){
    write-host("Commnad executed successfully")
    }else {
    write-host("Commnad execution failed ")
-   ExitWithCode 
+   exit $code
 }
 
  # remove default htm file
-  remove-item  C:\inetpub\wwwroot\ii.htm
+  remove-item  C:\inetpub\wwwroot\iisst.htm
   if($? -eq "true"){
    write-host("Commnad executed successfully")
   }else {
    write-host("Commnad execution failed ")
-   ExitWithCode 
+   exit $code
 }
 
  # Add a new htm file that displays server name
-  Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Hello Wor from " + $env:computername)
+  Add-Content -Path "C:\inetpb\ww\iisstart.htm" -Value $("Hello World from " + $env:computername)
   if($? -eq "true"){
    write-host("Commnad executed successfully")
    }else {
    write-host("Commnad execution failed ")
-   ExitWithCode 
+   exit $code 
 }
   
