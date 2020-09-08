4246712cc03d9b18635f795f5f8d4a9623ae0061
PradeepYadav1788 <33863962+PradeepYadav1788@users.noreply.github.com>
2020-09-08 19:28:19 +0530

    updated ps1 with exit code

--- iis.ps1
+++ iis.ps1
@@ -1,8 +1,32 @@
 # install IIS server role
+
+ function ExitWithCode { param($exitcode=99) $host.SetShouldExit($exitcode); exit }
+ 
+ 
  Install-WindowsFeature -name Web-Server -IncludeManagementTools
+ 
+ if($? -eq "true"){
+   write-host("Commnad executed successfully")
+   }else {
+   write-host("Commnad execution failed ")
+   ExitWithCode 
+}
 
  # remove default htm file
   remove-item  C:\inetpub\wwwroot\iisstart.htm
+  if($? -eq "true"){
+   write-host("Commnad executed successfully")
+  }else {
+   write-host("Commnad execution failed ")
+   ExitWithCode 
+}
 
  # Add a new htm file that displays server name
   Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Hello World from " + $env:computername)
+  if($? -eq "true"){
+   write-host("Commnad executed successfully")
+   }else {
+   write-host("Commnad execution failed ")
+   ExitWithCode 
+}
+  
